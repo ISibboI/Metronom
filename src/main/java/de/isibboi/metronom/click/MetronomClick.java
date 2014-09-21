f10790c0120606e4b012e11f789aa05355e3baed
Sebastian Schmidt <isibboi@gmail.com>
2014-09-21 12:13:31 +0200

    Added some attack to clicks.
    This was done to remove the harsh drum-like sound that is created by 0 to one changes within one sample.

--- src/main/java/de/isibboi/metronom/click/MetronomClick.java
+++ src/main/java/de/isibboi/metronom/click/MetronomClick.java
@@ -15,28 +15,35 @@ public abstract class MetronomClick {
 
 	public float[] createClick(final int sampleAmount) {
 		float[] line = lines.get(sampleAmount);
-		
+
 		if (line != null) {
 			return line;
 		}
-		
+
 		line = new float[sampleAmount];
-		
+
 		for (int i = 0; i < line.length; i++) {
 			float time = i / Metronom.SAMPLE_RATE;
 			time *= frequency;
 			time %= 1;
-			
-			line[i] = createSample(time, ((float)i) / (line.length - 1));
+
+			line[i] = createSample(time, ((float) i) / (line.length - 1));
+
+			// Add some attack time, and especially remove the click that is
+			// created by 0 to 1 changes within one sample.
+			if ((int) time == 0) {
+				line[i] *= time;
+			}
 		}
-		
+
 		lines.put(sampleAmount, line);
 		return line;
 	}
-	
+
 	public float getFrequency() {
 		return frequency;
 	}
-	
-	protected abstract float createSample(float positionInWave, float positionInLine);
+
+	protected abstract float createSample(float positionInWave,
+			float positionInLine);
 }
